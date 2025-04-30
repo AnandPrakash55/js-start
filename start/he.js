4edfdf0548fa8529e25e560fcab9be0bf6e00913
ANAND PRAKASH <122641115+AnandPrakash55@users.noreply.github.com>
2025-04-30 18:32:26 +0000

    stack

---
+++ start/he.js
@@ -0,0 +1,19 @@
+
+let myYoutubename = "rajasinghdotcom"
+let anothername = myYoutubename
+anothername = "oneside"
+
+console.log(myYoutubename)
+console.log(anothername)
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "anand@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
---
+++ start/va.js
@@ -0,0 +1,7 @@
+const accountId="5515"
+let accountEnail="gaganag@gmail.com"
+let accountPassword="rani"
+accountCity="Bangalore"
+
+console.log(accountEnail)
+console.table([accountId,accountPassword,accountCity])
