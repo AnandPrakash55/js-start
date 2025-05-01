e06f441f16823928b9300d6bd5ed774b85bd4a37
ANAND PRAKASH <122641115+AnandPrakash55@users.noreply.github.com>
2025-05-01 10:27:50 +0000

    string

---
+++ start/st.js
@@ -0,0 +1,19 @@
+const name = "raja"
+const repoCount = 50
+
+console.log('Hello my name is ${name} and my repo count is $ {repoCount}');
+
+const gameName = new String('NoobIgl')
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('I'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-0,4)
+console.log(anotherString);
+
+const newStringOne = "  Anand  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
