6e1165c36f676d03be04a632da2c216cb3f11706
ANAND PRAKASH <122641115+AnandPrakash55@users.noreply.github.com>
2025-05-14 12:53:33 +0000

    function

---
+++ start/fu.js
@@ -0,0 +1,21 @@
+console.log("A");
+console.log("n");
+console.log("a");
+console.log("n");
+console.log("d");
+
+function sayMyName(name){
+ console.log("A");
+ console.log("n");
+ console.log("a");
+ console.log("n");
+ console.log("d");
+}
+
+// 
+
+function addTwoNumbers(num1, num2){
+    console.log(num1 + num2);
+}
+
+addTwoNumbers(2, 3);
